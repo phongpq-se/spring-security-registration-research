ea8d1bb027bb1aad8397591e121f1fba60b10fd5
phongpq <v.phongpq@vinhomes.vn>
2025-03-10 23:26:49 +0700

    add simple test

---
+++ src/test/java/com/baeldung/phongpq/SimpleTest.java
@@ -0,0 +1,11 @@
+package com.baeldung.phongpq;
+
+import org.junit.jupiter.api.Test;
+
+public class SimpleTest {
+
+    @Test
+    void simpleTest() {
+        System.out.println("hello");
+    }
+}
